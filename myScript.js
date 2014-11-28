4de251e7564e320c71efe47d6787ab922c4c5dce
Auto Click <autoclick.us@gmail.com>
2014-11-28 13:58:14 +0700

    big update
    big update

---
+++ myScript.js
@@ -0,0 +1,9 @@
+﻿
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    if (request._method == "getHTML")
+        sendResponse({
+            _data: document.getElementById('browse_result_area').outerHTML,
+            _fullHtml: document.body.outerHTML
+        });
+  });
--- scripts/eventPage.js
+++
@@ -1,3 +0,0 @@
-﻿chrome.runtime.onInstalled.addListener(function () {
-    chrome.tabs.create({ url: 'http://www.autoclick.us/' });
-});
